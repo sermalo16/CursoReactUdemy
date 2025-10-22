b49538adf75ff6b1e7f71e6736827fdcf4cbfeb8
Sergio Morel <sersaml17@gmail.com>
2025-10-22 16:55:08 -0600

    Primer commit del curso React Udemy

---
+++ guitarla/src/App.jsx
@@ -0,0 +1,31 @@
+import Header from "./Components/Header"
+import Guitar from "./Components/Guitar"
+
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <Header/>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+            <Guitar/>
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default App
